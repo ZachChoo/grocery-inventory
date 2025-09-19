26147e9018fbb29bbd5506eb2d2401e8f780b75e
Zachary Choo <zachary.h.choo@gmail.com>
2025-09-19 13:58:16 -0700

    Imrpove error handling

--- app/models/user.py
+++ app/models/user.py
@@ -5,6 +5,6 @@ from app.database import Base
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
+    username = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
     role = Column(String, nullable=False)
